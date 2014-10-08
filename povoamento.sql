9a71fb58195d43f4049a1900837d1055e1308c02
uchoavaz <joaovictoruchoavaz@gmail.com>
2014-10-08 18:42:11 -0300

    CORRECAO

--- povoamento.sql
+++ povoamento.sql
@@ -1,9 +1,9 @@
-Insert into ATOR values ('01A','João','Brasileiro',21);
+insert into ATOR values ('01A','João','Brasileiro',21);
 
-Insert into FILME values ('01F','EU MORRI','01D');
+insert into FILME values ('01F','EU MORRI','01D');
 
-Insert into PARTICIPA values ('01A','01F');
+insert into PARTICIPA values ('01A','01F');
 
-Insert into DIRETOR values ('01D','Canabrava','01E');
+insert into DIRETOR values ('01D','Canabrava','01E');
 
-Insert into ESTUDIO values ('01E','Cana Producoes');
+insert into ESTUDIO values ('01E','Cana Producoes');
