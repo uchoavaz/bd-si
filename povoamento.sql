afc33b170df88af976bc880c6dfed45c99611ca3
uchoavaz <joaovictoruchoavaz@gmail.com>
2014-10-08 18:16:44 -0300

    Primeiro Exercicio

---
+++ povoamento.sql
@@ -0,0 +1,9 @@
+Insert into ATOR values ('01A','João','Brasileiro',21);
+
+Insert into FILME values ('01F','EU MORRI','01D');
+
+Insert into PARTICIPA values ('01A','01F');
+
+Insert into DIRETOR values ('01D','Canabrava','01E');
+
+Insert into ESTUDIO values ('01E','Cana Producoes');
